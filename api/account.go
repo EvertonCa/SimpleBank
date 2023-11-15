60d337b8b1fd7481f62835c6e3d1ba23a4dc0161
EvertonCa <eve.023@hotmail.com>
2023-11-14 22:22:04 -0300

    server get method and handlers

--- api/account.go
+++ api/account.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"database/sql"
+	"errors"
 	db "github.com/EvertonCa/SimpleBank/db/sqlc"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -26,6 +28,29 @@ func (server *Server) createAccount(ctx *gin.Context) {
 	account, err := server.store.CreateAccount(ctx, arg)
 	if err != nil {
 		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+	}
+
+	ctx.JSON(http.StatusOK, account)
+}
+
+type getAccountRequest struct {
+	ID int64 `uri:"id" binding:"required,min=1"`
+}
+
+func (server *Server) getAccount(ctx *gin.Context) {
+	var req getAccountRequest
+	if err := ctx.ShouldBindUri(&req); err != nil {
+		ctx.JSON(http.StatusBadRequest, errorResponse(err))
+	}
+
+	account, err := server.store.GetAccount(ctx, req.ID)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			ctx.JSON(http.StatusNotFound, errorResponse(err))
+			return
+		}
+		ctx.JSON(http.StatusInternalServerError, errorResponse(err))
+		return
 	}
 
 	ctx.JSON(http.StatusOK, account)
